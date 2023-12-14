c215433fe53a620c21d0c065790777fbf279bf92
AlaEldin <aeiedamo@gmail.com>
2023-12-15 01:18:31 +0300

    test

--- monty.h
+++ monty.h
@@ -1,12 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#define  _GNU_SOURCE
-
-#include <stdlib.h>
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -38,26 +39,46 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-typedef struct storage
-{
-        char mode;
-        char *buffer;
-        FILE *f;
-        char *arg;
-} storage;
+extern stack_t *head;
+typedef void (*op_func)(stack_t **, unsigned int);
 
-storage data = {'s', NULL, NULL, NULL};
+/*file operations*/
+void open_file(char *file_name);
+int parse_line(char *buffer, int line_number, int format);
+void read_file(FILE *);
+int len_chars(FILE *);
+void find_func(char *, char *, int, int);
 
-char *delimit = " \n\t";
+/*Stack operations*/
+stack_t *create_node(int n);
+void free_nodes(void);
+void print_stack(stack_t **, unsigned int);
+void add_to_stack(stack_t **, unsigned int);
+void add_to_queue(stack_t **, unsigned int);
 
-int _exe_(char *, stack_t **, unsigned int, FILE *);
-void free_struct(stack_t *head);
+void call_fun(op_func, char *, char *, int, int);
 
+void print_top(stack_t **, unsigned int);
+void pop_top(stack_t **, unsigned int);
+void nop(stack_t **, unsigned int);
+void swap_nodes(stack_t **, unsigned int);
 
-void _push_(stack_t **head, unsigned int n);
-void _pall_(stack_t **head, unsigned int n);
-void _push_queue_(stack_t **head, int n);
-void _push_stack_(stack_t **head, int n);
+/*Math operations with nodes*/
+void add_nodes(stack_t **, unsigned int);
+void sub_nodes(stack_t **, unsigned int);
+void div_nodes(stack_t **, unsigned int);
+void mul_nodes(stack_t **, unsigned int);
+void mod_nodes(stack_t **, unsigned int);
 
+/*String operations*/
+void print_char(stack_t **, unsigned int);
+void print_str(stack_t **, unsigned int);
+void rotl(stack_t **, unsigned int);
 
-#endif /*MONTY_H*/
+/*Error hanlding*/
+void err(int error_code, ...);
+void more_err(int error_code, ...);
+void string_err(int error_code, ...);
+void rotr(stack_t **, unsigned int);
+
+#endif
---
+++ test.m
@@ -0,0 +1,9 @@
+push 10
+push 20
+pall
+pop
+pop
+queue
+push 10
+push 20
+pall
